19c42f100c31b28b4ecebe902fa5626cf0ab76cd
Siphokazi Nyathi <siphokazinyathi@gmail.com>
2025-08-17 21:49:46 +0200

    corrected the App.jsx file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import './index.css'; // Make sure you import your main CSS file
+import RecipeDetail from './components/RecipeDetail';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
